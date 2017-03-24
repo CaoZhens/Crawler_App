981bff3dcd850eddda13a525808ab440c8f947e9
AlbertCAO <364296356@qq.com>
2017-03-24 16:22:04 +0800

    add rotate_useragent func

--- AppCrawler/settings.py
+++ AppCrawler/settings.py
@@ -14,7 +14,13 @@ BOT_NAME = 'AppCrawler'
 SPIDER_MODULES = ['AppCrawler.spiders']
 NEWSPIDER_MODULE = 'AppCrawler.spiders'
 
-
+# config msg of mongodb
+MONGO_HOST = "127.0.0.1"
+MONGO_PORT = 27017
+MONGO_DB = "Spider"
+MONGO_COLL = "baidu"
+MONGO_USER = "mySpider"
+MONGO_PSW = "mySpider123"
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'AppCrawler (+http://www.yourdomain.com)'
 
@@ -27,13 +33,16 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 0.5
+DOWNLOAD_HANDLERS = {
+    's3':None,
+}
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
@@ -52,9 +61,11 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'AppCrawler.middlewares.MyCustomDownloaderMiddleware': 543,
-#}
+
+# Using RotateUserAgentMiddleware
+DOWNLOADER_MIDDLEWARES = {
+    'AppCrawler.rotate_useragent.RotateUserAgentMiddleware': 400,
+}
 
 # Enable or disable extensions
 # See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
